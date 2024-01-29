707f43329f16cbf47c60b7a77cb861628f50ae95
copper280z <rday689@gmail.com>
2024-01-29 05:51:59 -0500

    moved motor class into lib and got pinout and basic config working

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,63 @@
 #include <Arduino.h>
 #include "pinout.h"
 #include "SimpleFOC.h"
-#include "HFIBLDCMotor.h"
 
-HFIBLDCMotor motor = HFIBLDCMotor(7);
+
+HFIBLDCMotor motor = HFIBLDCMotor(7,20);
 BLDCDriver3PWM driver = BLDCDriver3PWM(MOT1_A, MOT1_B, MOT1_C, MOT1_EN);
-LowsideCurrentSense currentsense = LowsideCurrentSense(0.01f, CS_A, CS_B);
+// BLDCDriver3PWM* pdriver = &driver;
+LowsideCurrentSense currentsense = LowsideCurrentSense(0.01f, 50.0f, CS_A, CS_B);
+// LowsideCurrentSense* pcurrentsense = &currentsense;
+
+Commander command = Commander(Serial);
+void onMotor(char* cmd){command.motor(&motor,cmd);}
+
+void process_hfi(){motor.process_hfi();}
+
+
 
 void setup() {
-  Serial.begin(250000);
+  pinMode(PC10,OUTPUT);
+  Serial.begin(2000000);
+	SimpleFOCDebug::enable(&Serial);
+  delay(1000);
 
-  driver.voltage_power_supply = 8;
+  driver.voltage_power_supply = 30;
+  driver.pwm_frequency = 30000;
+  // driver.voltage_limit = driver.voltage_power_supply/2;
+  driver.voltage_limit = driver.voltage_power_supply/2;
   driver.init();
 
+  command.add('M',&onMotor,"motor");
+
   motor.linkDriver(&driver);
+
+  motor.linkCurrentSense(&currentsense);
+  currentsense.linkDriver(&driver);
+  currentsense.init();
+
+  motor.LPF_current_d.Tf = 1/(1000*_2PI);
+  motor.LPF_current_q.Tf = 1/(1000*_2PI);
+  motor.torque_controller = TorqueControlType::foc_current;
+  // motor.controller = MotionControlType::velocity_openloop;
+  motor.controller = MotionControlType::torque;
+
+  motor.hfi_v = 8;
+
+  motor.init();
+  motor.initFOC();
+  motor.hfi_on = true;
+  delay(500);
+
 }
 
 void loop() {
+  motor.move();
+  motor.loopFOC();
+  command.run();
+  // Serial.println(motor.electrical_angle);
+  // Serial.print(motor.current_meas.d);
+  // Serial.print(", ");
+  // Serial.println(motor.current_meas.q);
+
 }
--- src/pinout.h
+++ src/pinout.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define MOT1_A  5
-#define MOT1_B  10
+#define MOT1_B  9
 #define MOT1_C  6
 #define MOT1_EN 8
 // #define MOT2_A  3
@@ -11,8 +11,8 @@
 // encoder pinouts
 #define ENC1_A 2
 #define ENC1_B 4
-#define ENC2_A A4
-#define ENC2_B 12
+// #define ENC2_A A4
+// #define ENC2_B 12
 
 #define CS_A A0
 #define CS_B A2
