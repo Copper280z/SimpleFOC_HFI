41487405ac980e3ffe2684fb2d5df8c57445fe6d
copper280z <rday689@gmail.com>
2024-03-03 23:38:57 -0500

    initial setup for dual motors

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 #include "pinout.h"
 #include "SimpleFOC.h"
 
-HFIBLDCMotor motor = HFIBLDCMotor(7,20);
+// prototypes
+void initFOC(HFIBLDCMotor *motor, BLDCDriver *driver, CurrentSense *currentsense);
+// prototypes
+
+HFIBLDCMotor motor_1 = HFIBLDCMotor(7,20);
+HFIBLDCMotor motor_2 = HFIBLDCMotor(7,20);
 
 #ifdef STM32F4xx
-BLDCDriver3PWM driver = BLDCDriver3PWM(MOT1_A, MOT1_B, MOT1_C, MOT1_EN);
-LowsideCurrentSense currentsense = LowsideCurrentSense(0.01f, 50.0f, CS_A, CS_B);
+BLDCDriver3PWM driver_1 = BLDCDriver3PWM(MOT1_A, MOT1_B, MOT1_C, MOT1_EN);
+LowsideCurrentSense currentsense_1 = LowsideCurrentSense(0.01f, 50.0f, CS1_A, CS1_B);
+
+BLDCDriver3PWM driver_2 = BLDCDriver3PWM(MOT2_A, MOT2_B, MOT2_C, MOT2_EN);
+LowsideCurrentSense currentsense_2 = LowsideCurrentSense(0.01f, 50.0f, CS2_A, CS2_B);
 #endif
 
 #ifdef ARDUINO_B_G431B_ESC1
@@ -22,9 +30,32 @@ LowsideCurrentSense currentsense = LowsideCurrentSense(0.01f, 50.0f, 35, 34);
 #endif
 
 Commander command = Commander(Serial);
-void onMotor(char* cmd){command.motor(&motor,cmd);}
+void onMotor_1(char* cmd){command.motor(&motor_1,cmd);}
+void onMotor_2(char* cmd){command.motor(&motor_2,cmd);}
+void target_2(char* cmd){command.scalar(&motor_2.target,cmd);}
 
-void process_hfi(){motor.process_hfi();}
+void process_hfi(int adc_index){
+  // if (adc_index==0){
+  //   motor_1.process_hfi();
+  //   digitalToggle(PC10);
+  //   digitalToggle(PC10);
+  // } else {
+  //   motor_2.process_hfi();
+  //   digitalToggle(PC10);
+  //   digitalToggle(PC10);
+  //   digitalToggle(PC10);
+  //   digitalToggle(PC10);
+  // }
+    digitalToggle(PC10);
+    digitalToggle(PC10);
+    motor_1.process_hfi();
+    digitalToggle(PC10);
+    digitalToggle(PC10);
+    motor_2.process_hfi();
+    digitalToggle(PC10);
+    digitalToggle(PC10);
+
+}
 
 
 
@@ -41,60 +72,79 @@ void setup() {
 
   while (!Serial.available()) {}
 
-  driver.voltage_power_supply = 33;
-  driver.pwm_frequency = 20000;
-  driver.voltage_limit = driver.voltage_power_supply*0.9;
-  driver.init();
+  command.add('M',&onMotor_1,"motor1");
+  command.add('N',&onMotor_2,"motor2");
+  command.add('T',&target_2,"T2");
 
-  command.add('M',&onMotor,"motor");
-
-  motor.linkDriver(&driver);
-
-  motor.linkCurrentSense(&currentsense);
-  currentsense.linkDriver(&driver);
-  // don't skip current sense align with sfoc shield
-  // currentsense.skip_align = true;
-  currentsense.init();
-
-  motor.current_limit = 0.3f;
-  motor.P_angle.P = 0.3f;
-  motor.P_angle.I = 0.1f;
-  motor.P_angle.D = 0.005f;
-  motor.P_angle.output_ramp = 0;
-  motor.LPF_angle.Tf = 0.;
-  motor.LPF_current_d.Tf = 1/(2000*_2PI);
-  motor.LPF_current_q.Tf = 1/(2000*_2PI);
-  motor.torque_controller = TorqueControlType::foc_current;
-  // motor.controller = MotionControlType::velocity_openloop;
-  motor.controller = MotionControlType::torque;
-
-  // 2804 140kv
-  motor.Ld = 2200e-6f;
-  motor.Lq = 3100e-6f;
+  Serial.println("Initializing motor 1");
+  initFOC(&motor_1, &driver_1, &currentsense_1);
+  motor_1.disable();
+  Serial.println("Initializing motor 1");
+  initFOC(&motor_2, &driver_2, &currentsense_2);
   
-  //5208 80kv
-  // motor.Ld = 4000e-6f;
-  // motor.Lq = 6500e-6f;
+  motor_1.enable();
 
-  motor.hfi_v = 10;
-
-  motor.init();
-  motor.initFOC();
-  
-  motor.hfi_on = true;
-  motor.sensor_direction = Direction::CCW;
-  motor.current_setpoint.d = 0.00f;
 }
 
 uint32_t time_prev=0;
 void loop() {
   uint32_t time_now = micros();
   if ((time_now-time_prev) > 1000){
-    motor.move();
+    motor_1.move();
+    motor_2.move();
     time_prev = time_now;
+    // noInterrupts();
+    // DQCurrent_s tmp = motor_2.current_meas;
+    // interrupts();
+    // Serial.printf("%.3f\n", tmp.d);
   }
 
-  motor.loopFOC();
+  motor_1.loopFOC();
+  motor_2.loopFOC();
   command.run();
 
+}
+
+void initFOC(HFIBLDCMotor *motor, BLDCDriver *driver, CurrentSense *currentsense) {
+  driver->voltage_power_supply = 33;
+  driver->pwm_frequency = 20000;
+  driver->voltage_limit = driver->voltage_power_supply*0.9;
+  driver->init();
+
+
+  motor->linkDriver(driver);
+
+  motor->linkCurrentSense(currentsense);
+  currentsense->linkDriver(driver);
+  // don't skip current sense align with sfoc shield
+  // currentsense.skip_align = true;
+  currentsense->init();
+
+  motor->current_limit = 0.3f;
+  motor->P_angle.P = 0.3f;
+  motor->P_angle.I = 0.1f;
+  motor->P_angle.D = 0.005f;
+  motor->P_angle.output_ramp = 0;
+  motor->LPF_angle.Tf = 0.;
+  motor->LPF_current_d.Tf = 1 / (2000 * _2PI);
+  motor->LPF_current_q.Tf = 1 / (2000 * _2PI);
+  motor->torque_controller = TorqueControlType::foc_current;
+  motor->controller = MotionControlType::torque;
+
+  // 2804 140kv
+  motor->Ld = 2200e-6f;
+  motor->Lq = 3100e-6f;
+  
+  //5208 80kv
+  // motor.Ld = 4000e-6f;
+  // motor.Lq = 6500e-6f;
+
+  motor->hfi_v = 12;
+
+  motor->init();
+  motor->initFOC();
+  
+  motor->hfi_on = true;
+  motor->sensor_direction = Direction::CCW;
+  motor->current_setpoint.d = 0.00f;
 }
--- src/pinout.h
+++ src/pinout.h
@@ -5,18 +5,23 @@
 #define MOT1_B  PC7 // 9 or 10 - PC7 or PB6
 #define MOT1_C  PB10 // 6, 11, or 13 - PB10, PA7, or PA5
 #define MOT1_EN PA9 // 8
-// #define MOT2_A  PB3 // 3
-// #define MOT2_B  PC7 // 9
-// #define MOT2_C  PA7 // 11
-// #define MOT2_EN PB10 // 7
+
+#define MOT2_A  PB3 // 3
+#define MOT2_B  PB6 // 10
+#define MOT2_C  PA7 // 11
+#define MOT2_EN PA8 // 7
+
 // encoder pinouts
 #define ENC1_A PA2 // 2
 #define ENC1_B PB3 // 4
 // #define ENC2_A A4
 // #define ENC2_B 12
 
-#define CS_A A0
-#define CS_B A2
+#define CS1_A A0
+#define CS1_B A2
+
+#define CS2_A PB_0_ALT1
+#define CS2_B PA_1_ALT1 
 #endif
 
 #ifdef ARDUINO_B_G431B_ESC1
